8665c632f836b6e671d1b14f990e180004ff294a
Audrey <audreymedaino@gmail.com>
2020-10-23 15:14:18 -0400

    Nav bar placement to the right with logo

--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Toolbar.scss';
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 
 const toolbar = props => (
   <header className="toolbar">
     <nav className="toolbar_navigation">
       <div>
+        <DrawerToggleButton/>
       </div>
       <div className="toolbar_logo">
         <a href="/"> <img className="photo" src={"https://static.wixstatic.com/media/d4dd4d_1cbc6f024a974132b0d5848f05b27135~mv2.jpg/v1/fill/w_343,h_228,al_c,q_80,usm_0.66_1.00_0.01/BLA_edited.webp"}/></a>
       </div>
+      <div className="spacer"/>
       <div className="toolbar_navigation_items">
         <ul>
           <li><a href="/">Biography</a></li>
