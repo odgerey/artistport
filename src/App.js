4f80e74f8b3a7d2eea2d101272b64fa24abbf735
Audrey <audreymedaino@gmail.com>
2020-10-23 11:59:59 -0400

    Navbar styling began

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Routes from './Routes/Routes.js';
-
+import Toolbar from './Components/Toolbar/Toolbar';
 
 function App() {
-  return <Routes/>
+  return (
+  <>
+  <Toolbar/>
+  <Routes/>
+  <main style={{marginTop: '64px'}}>
+    <p>This is content.</p>
+  </main>
+  </>
+  )
 }
 export default App;
 
---
+++ src/Components/Toolbar/Toolbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Toolbar.scss';
+
+const toolbar = props => (
+  <header className="toolbar">
+    <nav className="toolbar_navigation">
+      <div>
+      </div>
+      <div className="toolbar_logo">
+        <a href="/"> <img className="photo" src={"https://static.wixstatic.com/media/d4dd4d_1cbc6f024a974132b0d5848f05b27135~mv2.jpg/v1/fill/w_343,h_228,al_c,q_80,usm_0.66_1.00_0.01/BLA_edited.webp"}/></a>
+      </div>
+      <div className="toolbar_navigation_items">
+        <ul>
+          <li><a href="/">Biography</a></li>
+          <li><a href="/">Overview</a></li>
+          <li><a href="/">Work</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+); 
+
+export default toolbar;
