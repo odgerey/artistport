81a847d09cb36bb4689dcb571f94be085d1e149f
Audrey <audreymedaino@gmail.com>
2020-10-23 15:52:13 -0400

    Global index scss stylized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Routes from './Routes/Routes.js';
 import Toolbar from './Components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer'
 
 function App() {
   return (
   <>
-  <Toolbar/>
-  <Routes/>
-  <main style={{marginTop: '64px'}}>
-    <p>This is content.</p>
-  </main>
+  <div style={{height:'100%'}}>
+    <Toolbar/>
+    <SideDrawer/>
+    <Routes/>
+    <main style={{marginTop: '64px'}}>
+      <p>This is content.</p>
+    </main>
+  </div>
   </>
   )
 }
---
+++ src/Components/SideDrawer/SideDrawer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SideDrawer.css';
+const sideDrawer = props => (
+  <nav className="sideDrawer">
+    <ul>
+      <li>
+        <a href="/">Products</a>
+        <a href="/">Products</a>
+
+      </li>
+    </ul>
+  </nav>
+)
+
+export default sideDrawer;
